3b2aa376ff9643520ccc4343d28d03bfaed4e421
memowii <memogl25@gmail.com>
2020-09-09 11:58:24 -0500

    Displaying the Single Photo Component

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Comments = React.createClass({
+  render() {
+    
+    return (
+      <div className="comment">comment</div>
+    )
+  }
+});
+
+export default Comments;
